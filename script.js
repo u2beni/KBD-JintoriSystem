0530f0fe8226dafeb98de43a84bf2d63460af6d3
u2beni <xyamdis.arnet@gmail.com>
2023-03-26 01:43:51 +0900

    added json

---
+++ script.js
@@ -0,0 +1,7 @@
+var board_data = []; //盤面データ [x][y][{score,point,group,isLocked,isBase}]
+var player_data = []; //プレイヤーの名前、各機種のグループ分け JSONから読み込む
+var player_cost = []; //プレイヤーの残りコスト Interger
+
+$(document).ready(function () {
+    //ページ読み込み時の動作
+});
